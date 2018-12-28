00a16917368832e05e5e1ee5c8785bb79c7af2b2
Adrian Avram <avram.adrian.sorin@hotmail.co.uk>
2018-12-28 23:17:31 +0200

    crud operations

--- src/index.js
+++ src/index.js
@@ -1,51 +1,43 @@
 const { GraphQLServer } = require("graphql-yoga")
-const { Prisma } = require('prisma-binding')
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
 		feed: (root, args, context, info) => {
-			return context.db.query.links({}, info);
+			return context.prisma.links()
 		}
-		// link: (root, args) => links.find(link => link.id === args.id)
 	},
 	Mutation: {
 		post: (root, args, context, info) => {
-			return context.db.mutation.createLink(
-				{
-					data: {
-						url: args.url,
-						description: args.description
-					}
-				},
-				info
-			);
-		}
-		/*updateLink: (root, args) => {
-			const elem = links.findIndex(link => link.id === args.id)
-			links[elem].url = args.url
-			links[elem].description = args.description
-			return links.find(link => link.id === args.id)
+			return context.prisma.createLink({
+				url: args.url,
+				description: args.description
+			})
 		},
-		deleteLink: (root, args) => {
-			links.splice(links.findIndex(link => link.id === args.id),1)
-			return links.length > 0 ? links.find(link => link.id === args.id) : null
-		}*/
+		update: (root, args, context, info) => {
+			return context.prisma.updateLink({
+				data: {
+					url: args.url,
+					description: args.description
+				}, 
+				where: {
+					id: args.id 
+				}
+			})
+		},
+		delete: (root, args, context, info) => {
+			return context.prisma.deleteLink({
+				id: args.id 
+			})
+		}
 	}
 };
 
 const server = new GraphQLServer({
 	typeDefs: "./src/schema.graphql",
 	resolvers,
-	context: req => ({
-		...req,
-		db: new Prisma({
-			typeDefs: "src/generated/prisma.graphql",
-			endpoint: "http://localhost:4466",
-			secret: "mysecret123",
-			debug: true
-		})
-	})
+	context: { prisma }
 });
 
 server.start({ port: 5000 }, () =>
